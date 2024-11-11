80d70d469ff22de44378a02743a9d7710b91142f
AJMALAJU3 <ajmal.aju.340711@gmail.com>
2024-11-11 09:55:46 +0530

    z index ,scaling and hover issue resolved

--- src/components/HomePage/AnimeButton.jsx
+++ src/components/HomePage/AnimeButton.jsx
@@ -15,8 +15,8 @@ function AnimeButton() {
       <div className='flex justify-around max-w-sm md:w-96 w-72 z-10'>
 
 
-        <div className={`px-10 py-2 bg-gradient-to-r from-black text-gray-300 to-pink-500 rounded-full cursor-pointer ${isMounted ? 'opacity-100 scale-100' : 'opacity-0 w-0 scale-125'}
-        hover:border-t-4 shadow-custom-top-left  border-[#191919] font-bold  hover:shadow-black duration-1000 transition-all ease-in relative group`}>
+        <div className={`px-10 py-2 bg-gradient-to-r from-black text-gray-300 to-pink-500 rounded-full cursor-pointer ${isMounted ? 'opacity-100 ' : 'opacity-0 w-0 scale-125'}
+        hover:border-t-4 shadow-custom-top-left  border-[#191919] font-bold  hover:shadow-black duration-500 transition-all ease-in relative group`}>
             Explore
         <FaPen className='absolute text-black bottom-4 group-hover:opacity-100 z-[-1]
          opacity-0 duration-500 transition-all ease-in-out group-hover:bottom-16 group-hover:right-0 right-16 '/>
